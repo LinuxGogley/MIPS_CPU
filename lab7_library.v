096673c3ced035e961c7fde1c7bd4a174c013cc9
Dimitrios Paraschas <paraschas@gmail.com>
2013-11-27 20:51:59 +0200

    format Memory code

--- lab7_library.v
+++ lab7_library.v
@@ -38,33 +38,32 @@ module ALU (out, zero, inA, inB, op);
     end  // always
 endmodule
 
-// Memory (active 1024 words, from 10 address lsbs).
-// Read : enable ren, address addr, data dout
-// Write: enable wen, address addr, data din.
 module Memory (ren, wen, addr, din, dout);
-  input         ren, wen;
-  input  [31:0] addr, din;
-  output [31:0] dout;
+    // Memory (active 1024 words, from 10 address lsbs).
+    // Read : enable ren, address addr, data dout
+    // Write: enable wen, address addr, data din.
+    input ren, wen;
+    input [31:0] addr, din;
+    output [31:0] dout;
 
-  reg [31:0] data[4095:0];
-  wire [31:0] dout;
+    reg [31:0] data[4095:0];
+    wire [31:0] dout;
 
-  always @(ren or wen)
-    if (ren & wen)
-      $display ("\nMemory ERROR (time %0d): ren and wen both active!\n", $time);
+    always @(ren, wen)
+        if (ren & wen)
+            $display ("\nMemory ERROR (time %0d): ren and wen both active!\n", $time);
 
-  always @(posedge ren or posedge wen) begin
-    if (addr[31:10] != 0)
-      $display("Memory WARNING (time %0d): address msbs are not zero\n", $time);
-  end
+    always @(posedge ren, posedge wen) begin
+        if (addr[31:10] != 0)
+            $display("Memory WARNING (time %0d): address msbs are not zero\n", $time);
+    end
 
-  assign dout = ((wen==1'b0) && (ren==1'b1)) ? data[addr[9:0]] : 32'bx;
+    assign dout = ((wen == 1'b0) && (ren == 1'b1)) ? data[addr[9:0]] : 32'bx;
 
-  always @(din or wen or ren or addr)
-   begin
-    if ((wen == 1'b1) && (ren==1'b0))
-        data[addr[9:0]] = din;
-   end
+    always @(din, wen, ren, addr) begin
+        if ((wen == 1'b1) && (ren == 1'b0))
+            data[addr[9:0]] = din;
+    end
 endmodule
 
 module RegFile (clock, reset, raA, raB, wa, wen, wd, rdA, rdB);
