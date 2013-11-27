514b80b8a63965d8e585269a19b85657c54e1a08
Dimitrios Paraschas <paraschas@gmail.com>
2013-11-27 20:43:54 +0200

    copy ALU and RegFile from lab 6

--- lab7_library_input.v
+++ lab7_library_input.v
@@ -2,15 +2,40 @@
 
 `timescale 1ns/1ps
 
-// Small ALU. Inputs: inA, inB. Output: out.
-// Operations: bitwise and (op = 0)
-//             bitwise or  (op = 1)
-//             addition (op = 2)
-//             subtraction (op = 6)
-//             slt  (op = 7)
-//             nor (op = 12)
 module ALU (out, zero, inA, inB, op);
-    // to be completed in Lab6
+    // MIPS Arithmetic and Logic Unit
+    //
+    // opcodes
+    // -------
+    //  0 : bitwise AND      : out = inA & inB
+    //  1 : bitwise OR       : out = inA | inB
+    //  2 : addition         : out = inA + inB
+    //  6 : subtraction      : out = inA - inB
+    //  7 : set on less than : out = ((inA < inB) ? 1 : 0)
+    // 12 : bitwise NOR      : out = ~(inA | inB)
+
+    parameter N = 8;
+    output reg [N - 1:0] out;
+    output reg zero;
+    input [N - 1:0] inA, inB;
+    input [3:0] op;
+
+    always @(op) begin
+        case(op)
+            0000: out = inA & inB;
+            0001: out = inA | inB;
+            0010: out = inA + inB;
+            0110: out = inA - inB;
+            0111: out = ((inA < inB) ? 1 : 0);
+            1100: out = ~(inA | inB);
+            default: out = 'bx;
+        endcase
+
+        // TODO
+        // check this:
+        //zero = (out == 0);
+        zero = ((out == 0) ? 1 : 0);
+    end  // always
 endmodule
 
 // Memory (active 1024 words, from 10 address lsbs).
@@ -42,9 +67,38 @@ module Memory (ren, wen, addr, din, dout);
    end
 endmodule
 
-// Register File. Read ports: address raA, data rdA
-//                            address raB, data rdB
-//                Write port: address wa, data wd, enable wen.
 module RegFile (clock, reset, raA, raB, wa, wen, wd, rdA, rdB);
-    // To be completed in Lab6
+    // register file
+    //
+    // read ports
+    // ----------
+    // address raA, data rdA
+    // address raB, data rdB
+    //
+    // write ports
+    // -----------
+    // address wa, data wd, enable wen
+
+    input clock, reset;
+    input [4:0] raA, raB, wa;
+    input wen;
+    input [31:0] wd;
+    output reg [31:0] rdA, rdB;
+    reg [31:0] registers[0:31];
+    integer k;
+
+    always @(raA)
+        rdA = registers[raA];
+
+    always @(raB)
+        rdB = registers[raB];
+
+    always @(negedge reset)
+        for (k = 0; k < 32; k = k + 1)
+            registers[k] = 0;
+
+    always @(negedge clock)
+        if (reset != 0)
+            if (wen == 1)
+                registers[wa] = wd;
 endmodule
