92c31c6be6bd85471694b9f70cc171fa84abd93c
Dimitrios Paraschas <paraschas@gmail.com>
2013-11-27 17:56:02 +0200

    add files

---
+++ lab7_library_input.v
@@ -0,0 +1,57 @@
+`include "constants.h"
+`timescale 1ns/1ps
+
+// Small ALU. Inputs: inA, inB. Output: out. 
+// Operations: bitwise and (op = 0)
+//             bitwise or  (op = 1)
+//             addition (op = 2)
+//             subtraction (op = 6)
+//             slt  (op = 7)
+//             nor (op = 12)
+module ALU (out, zero, inA, inB, op);
+
+// to be completed in Lab6 
+
+endmodule
+
+
+// Memory (active 1024 words, from 10 address lsbs).
+// Read : enable ren, address addr, data dout
+// Write: enable wen, address addr, data din.
+module Memory (ren, wen, addr, din, dout);
+  input         ren, wen;
+  input  [31:0] addr, din;
+  output [31:0] dout;
+
+  reg [31:0] data[4095:0];
+  wire [31:0] dout;
+
+  always @(ren or wen)
+    if (ren & wen)
+      $display ("\nMemory ERROR (time %0d): ren and wen both active!\n", $time);
+
+  always @(posedge ren or posedge wen) begin
+    if (addr[31:10] != 0)
+      $display("Memory WARNING (time %0d): address msbs are not zero\n", $time);
+  end  
+
+  assign dout = ((wen==1'b0) && (ren==1'b1)) ? data[addr[9:0]] : 32'bx;
+  
+  always @(din or wen or ren or addr)
+   begin
+    if ((wen == 1'b1) && (ren==1'b0))
+        data[addr[9:0]] = din;
+   end
+
+endmodule
+
+
+// Register File. Read ports: address raA, data rdA
+//                            address raB, data rdB
+//                Write port: address wa, data wd, enable wen.
+module RegFile (clock, reset, raA, raB, wa, wen, wd, rdA, rdB);
+
+// To be completed in Lab6
+
+endmodule
+
